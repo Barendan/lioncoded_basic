5caf3835dd14564ca24332ef0fccb7c201308086
barendan <barendan@gmail.com>
2019-09-01 16:56:48 -0400

    clean code JS

--- app.js
+++ app.js
@@ -32,150 +32,174 @@ $(document).ready(function() {
 		br_cont2_title = $(".bottom_right .cont2 p"),
 		br_cont2_text = $(".bottom_right .cont2 h1"),
 
-		br_text_3 = $(".bottom_right .cont3"),
-		text_4 = $(".bottom_right .cont4"),
-
+		br_cont3 = $(".bottom_right .cont3"),
+		br_cont3_title = $(".bottom_right .cont3 p"),
+		br_cont3_text = $(".bottom_right .cont3 h1"),
 
 		myWork_section = $("section.mywork")
 		;
 
 
+
 	function init() {
+		console.log("hello buddy")
+		console.log(window.innerWidth);
+		window.innerWidth < 768 ? mobileAnimation() : desktopAnimation();
+	}
+
+
+	function desktopAnimation() {
+		// Pre-rendering
+		TweenMax.to(doWhat_background, .1, { opacity: 0, ease: Power0.easeIn }, 0)
+		
+		// Start animation
 		TweenMax.to(welcome, 1.5, {
 			opacity: 1,
 			delay: .5,
 			ease: Cubic.easeInOut,
-			onStart: function() {
-					// welcomeEnter()
-				},
-			onComplete: function() {
-					welcomeExit()
+			onStart: function() { sceneStart() },
+			onComplete: function() { aboutStart() }
+			}, 0)
+		
+		// Intro Scene
+		function sceneStart() {
+			let cloudTL = new TimelineMax({
+				delay: 1,
+				repeat: -1,
+				repeatDelay: 1
+			});
+			
+			cloudTL
+				.fromTo(scene_2, 15, {opacity: 0}, {opacity: 1},0)
+				.to(scene_2, 20, {x: '+=50', ease: Power0.easeNone },0)
+				.to(scene_2, 20, {x: '-=50', ease: Power0.easeNone },0);
+		}
+		// Exit Intro ; Start About
+		function aboutStart() {
+			const aboutStart = new TimelineMax();
+			aboutStart
+			.to(scene_2, 10, {y: "-300%", ease: Power0.easeIn}, 0)
+			.to(welcome_title, 5, {y: "-300%", ease: Power0.easeIn}, 0)
+			.to(scene_container, 5, {y: "-50%", scale:0.2, ease: Circ.easeIn}, 3)
+			.to(scene_container, 5, { scale: 0.5, ease: Power1.easeIn }, 3)
+			.to(welcome, 10, { opacity: 0, ease: Power2.easeOut }, 0)
+
+			.to(doWhat, 10, { opacity: 1, ease: Power0.easeIn }, 0)
+			.to(doWhat_background, 1, { opacity: 1, ease: Power0.easeIn }, 0)
+			.fromTo(doWhat_background, 5, { scale: 0.5 }, { scale: 1 }, 0)
+			
+			.fromTo(container_1, 10, {y: '100%'}, {y: '0%', ease: Sine.easeIn}, 5)
+			.fromTo(background_1, 10, {y: '-100%',opacity: 0}, {y: '0%',opacity: 0.5,ease: Sine.easeIn}, 5)
+			.fromTo(container_2, 10, {x: "-100%"}, {x: "0%",ease: Sine.easeInOut}, 5)
+			.fromTo(background_2, 10, {x: "100%",opacity: 0}, {
+				x: "0%",
+				opacity: 1,
+				ease: Sine.easeInOut,
+				onComplete: function() {
+					// TweenMax.to(br_cont1, 1, {opacity: 1, ease: Power2.easeIn})
 				}
-		}, 0)
-		TweenMax.to(doWhat_background, .1, { opacity: 0, ease: Power0.easeIn }, 0)
+			}, 5)
+			
+			.to(br_cont1, 5, {opacity: 1, ease: Sine.easeIn}, 10)
+			;
+				
+
+
+
+
+
+
+			let aboutEnd = new TimelineMax();
+			aboutEnd
+				// exit
+				.to(container_2, 2, {opacity: 0}, 1)
+				.to(container_2, 4, {x: "100%"}, 1)
+
+				// enter
+				.to(container_4, 1, {opacity: 1}, 1)
+				.fromTo(container_4, 4, {x: "-100%"}, {x: "0%"}, 1)
+				.fromTo(background_4, 4, {x: "95%",opacity: .5}, {x: "0%",opacity: 1}, 1)
+				
+				// exit
+				.to(br_cont1, 1, {opacity: 0}, 1)
+				.to(br_cont1_title, 1, {y: "200%", ease: Sine.easeInOut}, 1)
+				.to(br_cont1_text, 2, {y: "100%", ease: Sine.easeInOut}, 1)
+	
+
+				// enter
+				.to(br_cont2, 2, {opacity: 1, ease: Sine.easeIn}, 1.5)
+				.fromTo(br_cont2_title, 1, {y: "-200%"}, {y: "0%"}, 1.5)
+				.fromTo(br_cont2_text, 2, {y: "-100%"}, {y: "0%"}, 1.5)
+
+				// exit
+				.to(br_cont2, 1, {opacity: 0}, 1)
+				.to(br_cont2_title, 1, {y: "200%", ease: Sine.easeInOut}, 1)
+				.to(br_cont2_text, 2, {y: "100%", ease: Sine.easeInOut}, 1)
+
+				// enter
+				.to(br_cont3, 2, {opacity: 1, ease: Sine.easeIn}, 2)
+				.fromTo(br_cont3_title, 1, {y: "-200%"}, {y: "0%"}, 2)
+				.fromTo(br_cont3_text, 2, {y: "-100%"}, {y: "0%"}, 2)
 		
+		
+				.to(container_3, 1, {opacity: 1,ease: Sine.easeInOut}, 1)
+				.fromTo(container_3, 4, {y: "100%"}, {y: "0%",ease: Sine.easeInOut}, 1)
+				.fromTo(background_3, 4, {y: "-95%",opacity: .5}, {y: "0%",opacity: .5,ease: Sine.easeInOut, onComplete:function(){
+					// aboutEnd.to(br_cont1, 1, {opacity: 0, ease: Power1.easeIn},0)
+					// aboutEnd.to(br_cont2, 1, {opacity: 1, ease: Power1.easeIn},0)
+				}},1)
+				.to(container_5, 1, {opacity: 1,ease: Sine.easeInOut}, 5)
+				.fromTo(container_5, 5, {y: "100%"}, {y: "0%",ease: Sine.easeInOut}, 5)
+				
+	
+				.fromTo(background_5, 5, {y: "-95%",opacity: .0}, {y: "0%",opacity: 0.5,ease: Sine.easeInOut, onComplete:function(){
+					// aboutEnd.to(br_cont2, 1, {opacity: 0, ease: Power1.easeIn},0)
+					// aboutEnd.to(br_text_3, 1, {opacity: 1, ease: Power1.easeIn},0)
+				}}, 5)
+	
+	
+				.to(container_6, 1, {opacity: 1,ease: Sine.easeInOut}, 5)
+				.fromTo(container_6, 5, {x: "-100%"}, {x: "0%",ease: Sine.easeInOut}, 5)
+				.fromTo(background_6, 5, {x: "95%",opacity: .5}, {
+					x: "0%",
+					opacity: 1,
+					ease: Sine.easeInOut,
+					onComplete: function() {
+						myWork()
+					}
+				}, 5)
+	
+		
+
+			let enterWho = new ScrollMagic.Scene({
+				triggerElement: '.doWhat',
+				triggerHook: 1,
+				duration: '100%'
+			})
+			.setTween(aboutStart)
+			.addTo(controller);
+			
+			
+			let pinWho = new ScrollMagic.Scene({
+				triggerElement: '.doWhat',
+				triggerHook: 0,
+				duration: '100%'
+			})
+			.setPin('.doWhat')
+			.setTween(aboutEnd)
+			.addTo(controller);
+		}
 	}
 
-	function welcomeEnter() { 
-		let cloudTL = new TimelineMax({
-			delay: 1,
-			repeat: -1,
-			repeatDelay: 1
-		});
-		
-		cloudTL
-			.fromTo(scene_2, 15, {opacity: 0}, {opacity: 1},0)
-			.to(scene_2, 20, {x: '+=50', ease: Power0.easeNone },0)
-			.to(scene_2, 20, {x: '-=50', ease: Power0.easeNone },0);
-	}
 
-	function welcomeExit() {
-		const aboutStart = new TimelineMax();
-
-		aboutStart
-		.to(scene_2, 10, {y: "-300%", ease: Power0.easeIn}, 0)
-		.to(welcome_title, 5, {y: "-300%", ease: Power0.easeIn}, 0)
-		.to(scene_container, 5, {y: "-50%", scale:0.2, ease: Circ.easeIn}, 3)
-		.to(scene_container, 5, { scale: 0.5, ease: Power1.easeIn }, 3)
-		
-		.to(welcome, 10, { opacity: 0, ease: Power2.easeOut }, 0)
-		.to(doWhat, 10, { opacity: 1, ease: Power0.easeIn }, 0)
-
-		.to(doWhat_background, 1, { opacity: 1, ease: Power0.easeIn }, 0)
-		.fromTo(doWhat_background, 5, { scale: 0.5 }, { scale: 1 }, 0)
-		
-		.fromTo(container_1, 10, {y: '100%'}, {y: '0%', ease: Sine.easeIn}, 5)
-		.fromTo(background_1, 10, {y: '-100%',opacity: 0}, {y: '0%',opacity: 0.5,ease: Sine.easeIn}, 5)
-		.fromTo(container_2, 10, {x: "-100%"}, {x: "0%",ease: Sine.easeInOut}, 5)
-		.fromTo(background_2, 10, {x: "100%",opacity: 0}, {
-			x: "0%",
-			opacity: 1,
-			ease: Sine.easeInOut,
-			onComplete: function() {
-				// TweenMax.to(br_cont1, 1, {opacity: 1, ease: Power2.easeIn})
-			}
-		}, 5)
-		
-		.to(br_cont1, 5, {opacity: 1, ease: Sine.easeIn}, 10)
-		;
-			
-		let enterWho = new ScrollMagic.Scene({
-			triggerElement: '.doWhat',
-			triggerHook: 1,
-			duration: '100%'
-		})
-		.setTween(aboutStart)
-		.addTo(controller);
-
-		
-
-		let aboutEnd = new TimelineMax();
-		aboutEnd
-			// exit
-			.to(container_2, 2, {opacity: 0}, 1)
-			.to(container_2, 4, {x: "100%"}, 1)
-
-			// enter
-			.to(container_4, 1, {opacity: 1}, 1)
-			.fromTo(container_4, 4, {x: "-100%"}, {x: "0%"}, 1)
-			.fromTo(background_4, 4, {x: "95%",opacity: .5}, {x: "0%",opacity: 1}, 1)
-			
-			// exit
-			.to(br_cont1, 1, {opacity: 0}, 1)
-			.to(br_cont1_title, 1, {y: "200%", ease: Sine.easeInOut}, 1)
-			.to(br_cont1_text, 2, {y: "100%", ease: Sine.easeInOut}, 1)
-			
-
-			// enter
-			.to(br_cont2, 2, {opacity: 1, ease: Sine.easeIn}, 1.5)
-			.fromTo(br_cont2_title, 1, {y: "-200%"}, {y: "0%"}, 1.5)
-			.fromTo(br_cont2_text, 2, {y: "-100%"}, {y: "0%"}, 1.5)
-
-
-
-	    	.to(container_3, 1, {opacity: 1,ease: Sine.easeInOut}, 1)
-	    	.fromTo(container_3, 4, {y: "100%"}, {y: "0%",ease: Sine.easeInOut}, 1)
-	    	.fromTo(background_3, 4, {y: "-95%",opacity: .5}, {y: "0%",opacity: .5,ease: Sine.easeInOut, onComplete:function(){
-				// aboutEnd.to(br_cont1, 1, {opacity: 0, ease: Power1.easeIn},0)
-				// aboutEnd.to(br_cont2, 1, {opacity: 1, ease: Power1.easeIn},0)
-			}},1)
 
 
 
 
 			
-			// .to(container_5, 1, {opacity: 1,ease: Sine.easeInOut}, 5)
-			// .fromTo(container_5, 5, {y: "100%"}, {y: "0%",ease: Sine.easeInOut}, 5)
-			
 
-	    	// .fromTo(background_5, 5, {y: "-95%",opacity: .0}, {y: "0%",opacity: 0.5,ease: Sine.easeInOut, onComplete:function(){
-			// 	// aboutEnd.to(br_cont2, 1, {opacity: 0, ease: Power1.easeIn},0)
-			// 	// aboutEnd.to(br_text_3, 1, {opacity: 1, ease: Power1.easeIn},0)
-			// }}, 5)
+		
 
-
-
-	    	// .to(container_6, 1, {opacity: 1,ease: Sine.easeInOut}, 5)
-	    	// .fromTo(container_6, 5, {x: "-100%"}, {x: "0%",ease: Sine.easeInOut}, 5)
-			// .fromTo(background_6, 5, {x: "95%",opacity: .5}, {
-			// 	x: "0%",
-			// 	opacity: 1,
-			// 	ease: Sine.easeInOut,
-			// 	onComplete: function() {
-			// 		myWork()
-			// 	}
-			// }, 5)
-
-		let pinWho = new ScrollMagic.Scene({
-			triggerElement: '.doWhat',
-			triggerHook: 0,
-			duration: '100%'
-		})
-		.setPin('.doWhat')
-		.setTween(aboutEnd)
-		.addTo(controller);
-
-	}
 
 	function myWork() {
 		const paraCodeTween = new TimelineMax();
@@ -205,7 +229,6 @@ $(document).ready(function() {
 		}
 
 
-
 		const contactIntro = new TimelineMax();
 		contactIntro
 			.to('.contact_section', 5, {opacity: 1, ease: Power2.easeIn}, 0)
@@ -231,8 +254,12 @@ $(document).ready(function() {
 
 
 
+	// window.mobilecheck = function() {
+	// 	var check = false;
+	// 	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+	// 	return check;
+	//   };
 
-	
-	init();
 
+	init()
 })
